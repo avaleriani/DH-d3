8dd055920229ea000246f5f5ba137a17af1b52bf
Agustin Valeriani <agustin.valeriani@rga.com>
2018-02-26 17:37:53 -0300

    base for class 5

--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,19 @@
 import * as d3 from "d3";
 import styles from "../scss/styles.scss";
+import gMapsLoader from "load-google-maps-api";
 
+gMapsLoader.KEY = "AIzaSyAHs5pRSCrgEFaS_4L1s8lRijcDvFggNbI";
+gMapsLoader.LANGUAGE = "es";
+gMapsLoader.REGION = "AR";
 
-console.warning("test")
+gMapsLoader().then((googleMaps) => {
+  new googleMaps.Map(document.querySelector(".map"), {
+    center: {
+      lat: 40.7484405,
+      lng: -73.9944191
+    },
+    zoom: 12
+  })
+}).catch(function(error) {
+  console.error(error)
+});
