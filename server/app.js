fce54f56c4e4a94ce6fb3f808660c88d1e5e2acf
Agu Valeriani <agvaleriani@gmail.com>
2018-05-05 00:24:21 -0300

    updates

--- server/app.js
+++ server/app.js
@@ -309,10 +309,10 @@ app.use('/', function(req, res) {
 });
 
 // catch 404 and forward to error handler
-app.use(function (req, res, next) {
-    let err = new Error('Not Found');
-    err.status = 404;
-    next(err);
+app.use(function(req, res, next) {
+  let err = new Error('Not Found');
+  err.status = 404;
+  next(err);
 });
 //
 // app.use(function (err, req, res) {
